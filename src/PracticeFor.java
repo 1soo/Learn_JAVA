0e9d506c1e7ed4f9050e559122146feb758111f1
1soo <aaa4799@naver.com>
2022-07-25 17:15:02 +0900

    Practiice about for

---
+++ src/PracticeFor.java
@@ -0,0 +1,11 @@
+public class PracticeFor {
+    public static void main(String[] args){
+        int[] marks = { 90, 25, 67, 45, 80 };
+        for(int i = 0; i < marks.length; i++){
+            if(marks[i] > 60){
+                System.out.println("Student no." + (i + 1)
+                        + "Congratulation!! You are Passed.");
+            }
+        }
+    }
+}
