09d356ae6b6a25766a1bc70bd266f326f0b8d7d4
1soo <aaa4799@naver.com>
2022-07-26 09:29:45 +0900

    Practice about method

---
+++ src/PracticeMethod.java
@@ -0,0 +1,21 @@
+public class PracticeMethod {
+    int d;
+    int sum(int a, int b){
+        return a + b;
+    }
+    void addD(){
+        this.d++;
+    }
+    public static void main(String[] args){
+        int a = 3;
+        int b = 4;
+
+        PracticeMethod method = new PracticeMethod();
+        int c = method.sum(a, b);
+        method.d = 3;
+        method.addD();
+
+        System.out.println("a + b = " + c);
+        System.out.println("d = " + method.d);
+    }
+}
